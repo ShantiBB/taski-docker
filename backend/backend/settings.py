4ef21f496f4689efe0d3847cc1a82056246a670c
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-09-17 22:19:10 +1000

    fix pep8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,13 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = ('django-insecure-j_89af+30&&4qm*8z'
+              '9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.173.107', 'localhost', '127.0.0.1', 'taski-ever.zapto.org']
+ALLOWED_HOSTS = ['89.169.173.107', 'localhost',
+                 '127.0.0.1', 'taski-ever.zapto.org']
 
 
 # Application definition
@@ -77,16 +79,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 

1c21218f6bd43b8618aedcbf8a923c1eeb2303c2
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-09-18 11:27:50 +1000

    Add python-dotenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,18 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = ('django-insecure-j_89af+30&&4qm*8z'
-              '9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['89.169.173.107', 'localhost',
+ALLOWED_HOSTS = [os.getenv('IP_HOST'), 'localhost',
                  '127.0.0.1', 'taski-ever.zapto.org']
 
 
